0eb6f5b968fad3a20902e7273e788a2e6572392b
Zahra Pakdel <artimis.pakdel@gmail.com>
2023-02-02 21:26:20 +0330

    Header component connected to store and location

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,28 @@
 import { FaPlus, FaCaretDown } from "react-icons/fa";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import NewQuestionModal from "./NewQuestionModal";
+import { useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
 
-const mockData = {
-    profileName: "الناز شاکردوست",
-    profilePicUrl: "../../public/elnaz.png",
-    headerTitle: "لیست سوالات",
-}
+
 
 export default function Header() {
   const [showModal, setShowModal] = useState(false);
+  const [headerTitle, setHeaderTitle] = useState("")
+  const user = useSelector(state => state.user)
+  const location = useLocation()
+
+  useEffect(() => {
+    if (location.pathname.split("/")[1] == "question")
+      setHeaderTitle("جزییات سوال")
+    else
+      setHeaderTitle("لیست سوالات")
+  }, [location])
+
   return (
     <div>
       <nav className="navbar navbar-expand-lg py-4 px-14 bg-white relative flex items-center w-full justify-between">
-        <div className="font-bold text-2xl">{mockData.headerTitle}</div>
+        <div className="font-bold text-2xl">{headerTitle}</div>
         <div className="flex items-center">
           <button
             type="button"
@@ -25,10 +34,10 @@ export default function Header() {
           </button>
           <div className="flex justify-evenly mr-10 items-center">
             <img
-              src={mockData.profilePicUrl}
+              src={"/public/" + user.profilePicUrl}
               className="w-11 h-11 rounded-3xl"
             />
-            <span className="mx-4 text-sm">{mockData.profileName}</span>
+            <span className="mx-4 text-sm">{user.name}</span>
             <FaCaretDown />
           </div>
         </div>
