34090620478903b31e983abb296e299635d3356f
Zahra Pakdel <artimis.pakdel@gmail.com>
2023-02-02 23:04:29 +0330

    QuestionDetaisl updated, added AnswerForm

--- src/pages/QuestionDetails.jsx
+++ src/pages/QuestionDetails.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
-import Answers from '../components/Answers'
+import Answer from '../components/Answer'
+import AnswerForm from '../components/AnswerForm'
+import QuestionSummary from '../components/QuestionSummary'
 
 export default function QuestionDetails() {
   const { qid } = useParams()
   
   return (
-    <div>
-      <Answers/>
+    <div className="m-14">
+      <QuestionSummary showBody={true}/>
+      <Answer />
+      <AnswerForm />
     </div>
   )
 }
