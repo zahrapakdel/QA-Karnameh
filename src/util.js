fe5b2e8d74b59cd2dcdcda0f2f62b7578d885ab3
Zahra Pakdel <artimis.pakdel@gmail.com>
2023-02-02 19:10:12 +0330

    util file created, toFarsiNumber moved

---
+++ src/util.js
@@ -0,0 +1,9 @@
+export function toFarsiNumber(n) {
+  const farsiDigits = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"];
+
+  return n
+    .toString()
+    .split("")
+    .map((x) => farsiDigits[x])
+    .join("");
+}
