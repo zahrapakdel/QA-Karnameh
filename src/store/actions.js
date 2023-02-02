420f3659c4a09477728b1362b625a95c9b5451d9
Zahra Pakdel <artimis.pakdel@gmail.com>
2023-02-02 21:24:06 +0330

    Redux store created

---
+++ src/store/actions.js
@@ -0,0 +1,11 @@
+// Actions
+
+export const setActiveQuestion = "SET_ACTIVE_QUESTION"
+
+export const addAnswer = "ADD_ANSWER"
+
+export const addQuestion = "ADD_QUESTION"
+
+export const upVote = "UP_VOTE"
+
+export const downVote = "DOWN_VOTE"
---
+++ src/store/index.js
@@ -0,0 +1,41 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { addAnswer, addQuestion, setActiveQuestion, upVote, downVote } from "./actions";
+
+const initialState = {
+    user: {
+        name: "الناز شاکردوست",
+        profilePicUrl: "elnaz.png"
+    },
+    questions: [],
+    activeQuestion: {}
+}
+
+const reducer = (state=initialState, {type, payload}) => {
+    switch (type) {
+        case setActiveQuestion:
+            return {
+                ...state,
+                activeQuestion: payload.activeQuestion
+            }
+        case addAnswer:
+            var tmp = { ...state }
+            tmp.answers.push(payload.answer)
+            return tmp
+        case addQuestion:
+            var tmp = { ...state }
+            tmp.questions.push(pauload.question)
+            return tmp
+        case upVote:
+            var tmp = { ...state }
+            tmp.activeQuestion.upVotes += 1
+            return tmp
+        case downVote:
+            var tmp = { ...state }
+            tmp.activeQuestion.downVotes += 1
+            return tmp
+        default:
+            return state
+    }
+}
+
+export default configureStore({reducer})
